82aeeafa1d695049e5742ab76e3837007fc871ec
gab21el <gabrieloliveirassantos@gmail.com>
2026-06-03 11:10:08 -0300

    outra correção

--- src/com/Carros/entity/Carro.java
+++ src/com/Carros/entity/Carro.java
@@ -11,12 +11,15 @@ public class Carro {
     }
 
     public static void exibeCarros(Carro[] carros, int total) {
+        int cont = 0;
         double soma = 0;
         System.out.println("Carros cadastrados");
         for (int i = 0; i < total; i++) {
             carros[i].exibir();
             soma += carros[i].valor;
+            cont++;
         }
         System.out.println("A soma total é: " + soma);
+        System.out.println("O número de carros cadastrados é: " + cont);
     }
 }
